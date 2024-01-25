00eafa7df7ba9dc3d58882d5ffd8e073da5637fc
NadiasCodes <De.nadia@outlook.com>
2024-01-25 12:02:34 +0100

    made a new component to start implement the forecast for 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
+
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
   if (loaded) {
-    console.log(forecast)
+    console.log(forecast);
     return (
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <div className="WeatherForecast-day">Thu</div>
-            <WeatherIcon code="thunderstorm-day" size={45} />
-            <div className="WeatherForecast-temperatures">
-              {" "}
-              <span className="WeatherForecast-temperature-max">19°</span>
-              <span className="WeatherForecast-temperature-min">10°</span>
+           <WeatherForecastDay data={forecast[0]} />
             </div>
           </div>
         </div>
-      </div>
+      
     );
   } else {
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
     let apiKey = "57t510363ca64d76cf8d437ao0eea1eb";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
