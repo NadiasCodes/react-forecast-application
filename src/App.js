b5d1512a1086a1978850deaae6c44d50d23bea7e
NadiasCodes <De.nadia@outlook.com>
2024-01-29 12:16:45 +0100

    added the footer and social links and styled them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { BsGithub } from "react-icons/bs";
+import { BsWindowFullscreen } from "react-icons/bs";
+
+
 
 export default function App() {
   return (
@@ -8,12 +14,43 @@ export default function App() {
       <div className="container">
         <h2>Weather App</h2>
         <Weather defaultCity="Cologne" />
+
         <footer>
           This project was coded & designed by{" "}
-          <a href="https://www.linkedin.com/in/nadia-bouli/" target="blank" className="coder">
+          <a
+            href="https://www.linkedin.com/in/nadia-bouli/"
+            target="blank"
+            className="coder"
+          >
             Nadia Bouli
+            <br />
           </a>
-         
+         Hosting & GitHub <br />
+          <br />{" "}
+          <FontAwesomeIcon
+            icon={faArrowDown}
+            bounce
+            style={{ color: "#B197FC" }}
+          />
+          <br />
+          <div class="social-links d-flex justify-content-center mb-5">
+            <a
+              href="https://react-forecast-app.vercel.app/"
+              target="_blank"
+              title="Vercel"
+              rel="noopener noreferrer"
+            >
+              <BsWindowFullscreen className="VercelIcon" />
+            </a>
+            <a
+              href="https://github.com/NadiasCodes"
+              target="_blank"
+              title="GitHub Profile"
+              rel="noopener noreferrer"
+            >
+              <BsGithub className="githubIcon" />
+            </a>
+          </div>
         </footer>
       </div>
     </div>
