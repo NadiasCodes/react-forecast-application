be187c98328174bae84bcdf526d3843548a172e4
NadiasCodes <De.nadia@outlook.com>
2024-03-04 09:30:33 +0100

    adjustments to the footer and fixed the format for the time

--- src/App.js
+++ src/App.js
@@ -20,43 +20,7 @@ export default function App() {
         </p>
         <Weather defaultCity="Cologne" />
 
-        <footer>
-          This project was coded & designed by{" "}
-          <a
-            href="https://www.linkedin.com/in/nadia-bouli/"
-            target="blank"
-            className="coder"
-          >
-            Nadia Bouli
-            <br />
-          </a>
-          Hosting & GitHub <br />
-          <br />{" "}
-          <FontAwesomeIcon
-            icon={faArrowDown}
-            bounce
-            style={{ color: "#B197FC" }}
-          />
-          <br />
-          <div class="social-links d-flex justify-content-center mb-5">
-            <a
-              href="https://react-forecast-app.vercel.app/"
-              target="_blank"
-              title="Vercel"
-              rel="noopener noreferrer"
-            >
-              <BsWindowFullscreen className="VercelIcon" />
-            </a>
-            <a
-              href="https://github.com/NadiasCodes"
-              target="_blank"
-              title="GitHub Profile"
-              rel="noopener noreferrer"
-            >
-              <BsGithub className="githubIcon" />
-            </a>
-          </div>
-        </footer>
+        
       </div>
     </div>
   );
